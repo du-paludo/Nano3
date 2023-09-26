c2499bd01b47b95360c5b05f42b5685285d77471
Eduardo Paludo <eduardo.paludo8@gmail.com>
2023-09-26 09:02:07 -0300

    Initial Commit

---
+++ Nano 3/Nano_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Nano_3App.swift
+//  Nano 3
+//
+//  Created by Eduardo Stefanel Paludo on 26/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Nano_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
